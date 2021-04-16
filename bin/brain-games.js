0b0088604bc64e9f6d75b8cc52cd853c97e489dc
Астемир Тлюстангелов <tlustangelov99@mail.ru>
2021-04-16 13:46:53 +0300

    приветствие и подключение библиотеки

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 console.log("Welcome to the Brain Games!");
 
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('May I have your name?');
+
+console.log(`Hello, ${name}!`); 
