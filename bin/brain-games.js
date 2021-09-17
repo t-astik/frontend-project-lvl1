cdd6c5e4186ba83d5fec885add77a423d5611ee5
Астемир Тлюстангелов <tlustangelov99@mail.ru>
2021-09-17 14:46:57 +0300

    test

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,9 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 
-console.log("Welcome to the Brain Games!");
+// console.log( "Welcome to the Brain Games!" );
 
-import readlineSync from 'readline-sync';
+// import readlineSync from 'readline-sync';
 
-const name = readlineSync.question('May I have your name?');
+// const name = readlineSync.question('May I have your name?');
 
-console.log(`Hello, ${name}!`)
+// console.log(`Hello, ${name}!`)
