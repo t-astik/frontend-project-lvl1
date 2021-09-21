b3955a887d7b10f1c70772971c2e34c98f56148c
Астемир Тлюстангелов <tlustangelov99@mail.ru>
2021-09-21 13:53:22 +0300

    finished version of game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -9,23 +9,34 @@ console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const randomNumber = getRandomInt();
-console.log(`Question: ${randomNumber}`);
-
-const myAnswer = readlineSync.question('Your answer: ');
-
-if ((myAnswer === 'yes' && randomNumber % 2 === 0) || (myAnswer === 'no' && randomNumber % 2 !== 0)) {
-  console.log('Correct!');
-} else if (myAnswer === 'yes') {
-  console.log("'yes' is wrong answer ;(. Correct answer was 'no'");
-  console.log(`Let's try again, ${name}`);
-} else if (myAnswer === 'no') {
-  console.log("'no' is wrong answer ;(. Correct answer was 'yes'");
-  console.log(`Let's try again, ${name}`);
-} else if (myAnswer !== 'yes' && myAnswer !== 'no' && randomNumber % 2 === 0) {
-  console.log(`'${myAnswer}' is wrong answer ;(. Correct answer was 'yes'`);
-  console.log(`Let's try again, ${name}`);
-} else if (myAnswer !== 'yes' && myAnswer !== 'no' && randomNumber % 2 !== 0) {
-  console.log(`'${myAnswer}' is wrong answer ;(. Correct answer was 'no'`);
-  console.log(`Let's try again, ${name}`);
+let right = true;
+let count = 0;
+while (right === true && count < 3) {
+  const randomNumber = getRandomInt();
+  console.log(`Question: ${randomNumber}`);
+  const myAnswer = readlineSync.question('Your answer: ');
+  if ((myAnswer === 'yes' && randomNumber % 2 === 0) || (myAnswer === 'no' && randomNumber % 2 !== 0)) {
+    console.log('Correct!');
+    right = true;
+    count += 1;
+    if (count === 3) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  } else if (myAnswer === 'yes') {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'");
+    console.log(`Let's try again, ${name}`);
+    right = 2;
+  } else if (myAnswer === 'no') {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'");
+    console.log(`Let's try again, ${name}`);
+    right = 2;
+  } else if (myAnswer !== 'yes' && myAnswer !== 'no' && randomNumber % 2 === 0) {
+    console.log(`'${myAnswer}' is wrong answer ;(. Correct answer was 'yes'`);
+    console.log(`Let's try again, ${name}`);
+    right = 2;
+  } else if (myAnswer !== 'yes' && myAnswer !== 'no' && randomNumber % 2 !== 0) {
+    console.log(`'${myAnswer}' is wrong answer ;(. Correct answer was 'no'`);
+    console.log(`Let's try again, ${name}`);
+    right = 2;
+  }
 }
